0574bc152f8e56694fbf7aecab5a8a4795344d47
KafCoppelia <ubuntu@localhost.localdomain>
2022-01-14 16:25:56 +0800

    更新pip或poetry安装🥳

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# nonebot_plugin_roll
-Simple function of rolling for nonebot v2 from nonebot_instant_plugins
+<div align="center">
+
+# Roll
+
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable-next-line MD036 -->
+_✨ 扔骰(tóu)子 ✨_
+<!-- prettier-ignore-end -->
+
+</div>
+
+<p align="center">
+  
+  <a href="https://github.com/KafCoppelia/nonebot_plugin_roll/blob/main/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-informational">
+  </a>
+  
+  <a href="https://github.com/nonebot/nonebot2">
+    <img src="https://img.shields.io/badge/nonebot2-2.0.0beta.1-green">
+  </a>
+  
+  <a href="">
+    <img src="https://img.shields.io/badge/release-v0.1.0-orange">
+  </a>
+  
+</p>
+
+</p>
+
+## 版本
+
+v0.1.0
+
+## 安装
+
+1. 通过`pip`或`poetry`安装；
+
+2. Enjoy:tada:
+
+## 功能
+
+掷骰！扔出指定个数的多面骰子。
+
+## 命令
+
+rd、roll、掷骰 + <x>d<y>， x指定个数，y指骰子面数。
+
+## 本插件改自Omega-Miya roll点抽奖插件
+
+[Omega Miya](https://github.com/Ailitonia/omega-miya)
---
+++ nonebot_plugin_roll/__init__.py
@@ -0,0 +1,77 @@
+import re
+import random
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.event import GroupMessageEvent
+from nonebot.adapters.cqhttp.permission import GROUP
+
+roll = on_command(
+    'roll',
+    # 使用run_preprocessor拦截权限管理, 在default_state初始化所需权限
+    state={
+        '_matcher_name': 'roll',
+        '_command_permission': True,
+        '_permission_level': 10,
+        '_cool_down': 5
+    },
+    aliases={'Roll', '掷骰子', '掷骰', 'rd'},
+    permission=GROUP,
+    priority=10,
+    block=True)
+
+
+# 修改默认参数处理
+@roll.args_parser
+async def parse(bot: Bot, event: GroupMessageEvent, state: T_State):
+    args = str(event.get_plaintext()).strip().lower().split()
+    if not args:
+        await roll.reject('你似乎没有发送有效的参数, 请重新发送:')
+    state[state["_current_key"]] = args[0]
+    if state[state["_current_key"]] == '取消':
+        await roll.finish('操作已取消')
+
+
+@roll.handle()
+async def handle_first_receive(bot: Bot, event: GroupMessageEvent, state: T_State):
+    args = str(event.get_plaintext()).strip().lower().split()
+    if not args:
+        pass
+    elif args and len(args) == 1:
+        state['roll'] = args[0]
+    else:
+        await roll.finish('参数错误QAQ')
+
+
+@roll.got('roll', prompt='请掷骰子: <x>d<y>')
+async def handle_roll(bot: Bot, event: GroupMessageEvent, state: T_State):
+    _roll = state['roll']
+    if re.match(r'^\d+[d]\d+$', _roll):
+        # <x>d<y>
+        dice_info = _roll.split('d')
+        dice_num = int(dice_info[0])
+        dice_side = int(dice_info[1])
+    elif re.match(r'^[d]\d+$', _roll):
+        # d<x>
+        dice_num = 1
+        dice_side = int(_roll[1:])
+    elif re.match(r'^\d+$', _roll):
+        # Any number
+        dice_num = 1
+        dice_side = int(_roll)
+    else:
+        await roll.finish(f'格式不对呢, 请重新输入: /roll <x>d<y>:')
+        return
+
+    # 加入一个趣味的机制
+    if random.randint(1, 100) == 99:
+        await roll.finish(f'【彩蛋】骰子之神似乎不看好你, 你掷出的骰子全部消失了😥')
+    if dice_num > 1024 or dice_side > 1024:
+        await roll.finish(f'【错误】谁没事干扔那么多骰子啊😅')
+    if dice_num <= 0 or dice_side <= 0:
+        await roll.finish(f'【错误】你掷出了不存在的骰子, 只有上帝知道结果是多少🤔')
+    dice_result = 0
+    for i in range(dice_num):
+        this_dice_result = random.choice(range(dice_side)) + 1
+        dice_result += this_dice_result
+    await roll.finish(f'你掷出了{dice_num}个{dice_side}面骰子, 点数为【{dice_result}】')
